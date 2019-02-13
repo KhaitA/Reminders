5867cc75c12907899ac2607b52b40626106a5a2e
KhaitA <44742857+KhaitA@users.noreply.github.com>
2019-02-13 14:31:23 +0200

    Update reminders2.py
    Я начала Web Scraping страницы с Reminders. Столкнулась с отсутствием доступа к ней. Нашла, как обойти пароль на стандартных сайтах. Однако с Inbox by Google стандартный метод:
    import requests
    
    #This URL will be the URL that your login form points to with the "action" tag.
    POST_LOGIN_URL = 'https://my.freecycle.org/login'
    
    #This URL is the page you actually want to pull down with requests.
    REQUEST_URL = 'https://my.freecycle.org/home/posts'
    
    
    #username-input-name is the "name" tag associated with the username input field of the login form.
    #password-input-name is the "name" tag associated with the password input field of the login form.
    payload = {
        'username-input-name': 'username',
    	'password-input-name': 'password'  #Preferably set your password in an env variable and sub it in.
    }
    
    with requests.Session() as session:
        post = session.post(POST_LOGIN_URL, data=payload)
    	r = session.get(REQUEST_URL)
    	print(r.text)   #or whatever else you want to do with the request data!
    
    не работает (причину указала в комментариях к коду).

--- reminders2.py
+++ reminders2.py
@@ -5,12 +5,16 @@ Created on Wed Feb 13 12:05:59 2019
 @author: User
 """
 
-
+#Импортирую библиотеки необходимые для Scraping
 
 import requests
 
 from bs4 import BeautifulSoup
 
+#код ниже должен мне помочь зайти непосредственно на страницу Reminders в Inbox (в идеале: .post проходит процедуру авторизации, а .get дает нам нужную страницу).
+# Однако тут возникла загвоздка. Большинство tutorials показывают, как авторизоваться на сайтах, где username&password находятся на 1 странице
+# Google же сначала на 1 странице просит ввести логин, далее перенаправляет на следующую страницу с паролем.
+# У меня не получается попасть на страницу Reminders, ни стандартным способом, ни тем, что описан ниже
 
 s = requests.session()
 email='username'
@@ -29,12 +33,15 @@ k = l.get('https://inbox.google.com/reminders')
 
 #print(k.content[:100])
 
-
+# Parsing web page
 soup = BeautifulSoup(k.content, 'html.parser')
 print(soup.prettify())
 
+# Find reminders on the page
 div = soup.find('div')
 #print(div.prettify())
 reminders = div.find("div", class_= "tE")
+
+# Далее не могу написать код, так в консоли не отображается нужная мне страница
 
 
